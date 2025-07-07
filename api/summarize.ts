3c5e27197f4e491c3ebb370de6e845c845c074b9
AshevilleHabitat <dmendl@ashevillehabitat.org>
2025-07-07 13:44:23 -0400

    pdf parse library fork

--- api/summarize.ts
+++ api/summarize.ts
@@ -1,7 +1,7 @@
 // Vercel deploys files in the /api directory as serverless functions.
 // This function will be accessible at the `/api/summarize` endpoint.
 
-import pdf from "pdf-parse";
+import pdf from "pdf-parse-fork-team";
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { GoogleGenAI, GenerateContentResponse } from "@google/genai";
 import * as cheerio from 'cheerio';
